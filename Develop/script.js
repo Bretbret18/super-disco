15e0a4b25f0098772e714df8074908ebd97f6280
Bret Sperry <bret.sperry@gmail.com>
2020-12-11 18:54:11 -0500

    finishing commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,44 @@
-let date = moment().format('dddd, MMMM Do')
 
-$(document).ready(function() {
-    $('#currentDay').text(date)
 
-    for(var i =0; i < 5; i++) {
-        $('.container').append(`     
-        <div class="row">
-            <div class="col-2">
-            
-            </div>
-            <div class="col-8">
-            <textarea> event goes here</textarea>
-            </div>
-            <div class="col-2">
-            <i class="fas fa-save"></i>
-            </div>
-      </div>`)
-    }
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    var currentHour = moment().hour();
+
+
+    //save planner text
+    $('.saveBtn').click(function () {
+        console.log('button clicked')
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id');
+
+        localStorage.setItem(time, value);
+    });
+
+
+    //display saved text
+    $('textarea').each(function () {
+        var time = $(this).parent().attr('id');
+        var scheduleTime = localStorage.getItem(time);
+        $(this).val(scheduleTime);
+
+    });
+
+
+
+    //apply time colors
+    $('.time-block').each(function () {
+        var id = parseInt($(this).attr('id'));
+        if (id < currentHour) {
+            $(this).addClass('past');
+        } else if (id === currentHour) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    })
+
+
 })
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Work Day Scheduler Starter Code
+This is a work day planner that provides the user with the current date as well as 
+columns that represent each hour of the work day. Users can schedule and save information
+for each hour. The past, present, and future time is highlighted with assigned colors to 
+make the user aware of time! Information added by the user is saved in local storage and
+will remain, regardless if the page is refreshed. 
